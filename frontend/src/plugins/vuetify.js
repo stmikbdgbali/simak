7b731188095eedfbfc8582a3345852c020074bad
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2021-02-19 08:36:15 +0700

    .environtment

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -3,4 +3,18 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+	theme: {
+		themes: {
+			light: {
+				primary: "#2196f3",
+				secondary: "#03a9f4",
+				accent: "#ffeb3b",
+				error: "#f44336",
+				warning: "#ff9800",
+				info: "#00bcd4",
+				success: "#4caf50",
+			},
+		},
+	},
+});
